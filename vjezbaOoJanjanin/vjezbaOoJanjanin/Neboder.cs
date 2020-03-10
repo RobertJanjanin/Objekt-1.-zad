bf80079fc73d4416184e295512ce6eec50e3d44a
RobertJanjanin <55134583+RobertJanjanin@users.noreply.github.com>
2020-03-10 11:46:17 +0100

    Add files via upload

---
+++ vjezbaOoJanjanin/vjezbaOoJanjanin/Neboder.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vjezbaOoJanjanin
+{
+    class Neboder
+    {
+        private int visina;
+        private int brojKatova;
+        public Neboder()
+        {
+
+
+        }
+        public void setVisina(int visina)
+        {
+            this.visina = visina;
+        }
+        public void setBrojKatova(int brojKatova)
+        {
+            this.brojKatova = brojKatova;
+        }
+
+        public int getVisina()
+        {
+            return visina;
+
+        }
+        public int getBrojKatova() {
+            return brojKatova;
+
+        }
+        public override string ToString()
+        {
+            string ispis = "visina nebodera: " + visina + "\nBroj Katova: " + brojKatova;
+               
+            return ispis;
+        }
+        public Neboder(int visina, int brojKatova) {
+            this.visina = visina;
+            this.brojKatova = brojKatova;
+        }
+    }
+}
---
+++ vjezbaOoJanjanin/vjezbaOoJanjanin/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vjezbaOoJanjanin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float prosjek;
+            Neboder Tower = new Neboder(200, 100);                     
+            Console.WriteLine(Tower.ToString());
+            Tower.setVisina(Tower.getVisina() + 10);
+            Tower.setBrojKatova(Tower.getBrojKatova() + 1);
+            Console.WriteLine(Tower.ToString());
+            prosjek = Tower.getVisina() / Tower.getBrojKatova();
+            Console.WriteLine("Prosječna visina kata je: "+prosjek);
+            Console.ReadKey();
+        }
+    }
+}
